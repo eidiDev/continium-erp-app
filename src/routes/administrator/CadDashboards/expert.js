58bed96219d7f803e40f5f5a6a3d80d90e7d9996
EidiKumagai <lucaseidikumagai@gmail.com>
2022-01-31 18:31:46 -0300

    Update expert.js

--- src/routes/administrator/CadDashboards/expert.js
+++ src/routes/administrator/CadDashboards/expert.js
@@ -154,15 +154,15 @@ class CadDashboardsexpert extends Component {
   onHandleClickSave = () => {
     // console.log(this.state.model);
 
-    let isok = 0;
-    var pwd = prompt("Para alterar o valor é necessário a senha.");
-    if(pwd != null && pwd === 'COTINIUMERP@') {
-        isok = 1;
-    }else {
-        message.error('Senha incorreta!');
-    }
+    // let isok = 0;
+    // var pwd = prompt("Para alterar o valor é necessário a senha.");
+    // if(pwd != null && pwd === 'COTINIUMERP@') {
+    //     isok = 1;
+    // }else {
+    //     message.error('Senha incorreta!');
+    // }
     
-    if(!isok) { return };
+    // if(!isok) { return };
 
     let record = this.state.user;
     var parent = this;
@@ -214,15 +214,15 @@ class CadDashboardsexpert extends Component {
 
   onHandleClickDelete = () => {
     // console.log(this.state.model);
-    let isok = 0;
-    var pwd = prompt("Para alterar o valor é necessário a senha.");
-    if(pwd != null && pwd === 'COTINIUMERP@') {
-        isok = 1;
-    }else {
-        message.error('Senha incorreta!');
-    }
+    // let isok = 0;
+    // var pwd = prompt("Para alterar o valor é necessário a senha.");
+    // if(pwd != null && pwd === 'COTINIUMERP@') {
+    //     isok = 1;
+    // }else {
+    //     message.error('Senha incorreta!');
+    // }
     
-    if(!isok) { return };
+    // if(!isok) { return };
 
     let record = this.state.user;
     var parent = this;
